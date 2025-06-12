9ca6e835f9be3ef80a7bd80ddd4a197abc2cc608
Musa <zarifanovmusa@gmail.com>
2025-06-13 00:45:21 +0300

    first commit

---
+++ FinanceAppYandex/FinanceAppYandexApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppYandexApp.swift
+//  FinanceAppYandex
+//
+//  Created by Муса Зарифянов on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppYandexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanceAppYandexTests/FinanceAppYandexTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppYandexTests.swift
+//  FinanceAppYandexTests
+//
+//  Created by Муса Зарифянов on 13.06.2025.
+//
+
+import Testing
+@testable import FinanceAppYandex
+
+struct FinanceAppYandexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
